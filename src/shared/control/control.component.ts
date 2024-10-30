fc932861f72eeb590f78b2d85c69ebe3124a2ea0
akash <sunder.akash2000@gmail.com>
2024-10-30 10:50:48 +0530

    Initial commit

---
+++ src/shared/control/control.component.ts
@@ -0,0 +1,25 @@
+import { Component, ElementRef, HostBinding, HostListener, inject, input, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-control',
+  standalone: true,
+  imports: [],
+  templateUrl: './control.component.html',
+  styleUrl: './control.component.css',
+  encapsulation: ViewEncapsulation.None,
+  host:{
+    class: 'control'
+  }
+})
+export class ControlComponent {
+
+  label = input.required<string>();
+
+  private element = inject(ElementRef);
+
+  @HostListener('click') onClicked(){
+    console.log(this.element);
+  
+  }
+
+}
